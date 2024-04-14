886bd2b15eda54eeb49fb2bf2e6f6e5d42c152b6
tomoki-yamamura <progtab17@gmail.com>
2024-04-14 18:19:20 +0800

    filter obj date

--- src/entities/row.ts
+++ src/entities/row.ts
@@ -9,6 +9,6 @@ export class Row {
   ) {}
 }
 
-type DynamicKeys = {
+export type DynamicKeys = {
   [key in Users]: string;
 };
--- src/repositories/rowRepository.ts
+++ src/repositories/rowRepository.ts
@@ -2,7 +2,7 @@ import { GoogleSpreadsheet } from "google-spreadsheet";
 import { IRowRepository } from "../interfaces/IRowRepository";
 import { gsDoc } from "../google-sheet-config";
 import { injectable } from "inversify";
-import { Row } from "../entities/row";
+import { DynamicKeys, Row } from "../entities/row";
 
 @injectable()
 export class RowRepository implements IRowRepository {
@@ -23,17 +23,28 @@ export class RowRepository implements IRowRepository {
   }): Promise<Row[]> {
     await this.doc.loadInfo();
     console.log(id, start_date, end_date);
-    
+
     const sheet = this.doc.sheetsByIndex[Number(id)];
     const rows = await sheet.getRows();
     // if you have domain logic, you should use it first.
-    rows.map((row) => {
-      const obj = row.toObject();
-      return obj.Date >= start_date && obj.Date <= end_date;
-    });
-    return rows.map((row) => {
-      const obj = row.toObject();
-      return new Row(obj.ID, obj.Date, obj.Timestamp, obj.HASSYHHH);
-    });
+    return rows
+      .filter((row) => {
+        const obj = row.toObject();
+        console.log(start_date, end_date, obj.Date);
+        
+        return obj.Date >= start_date && obj.Date <= end_date;
+      })
+      .map((row) => {
+        const obj = row.toObject();
+        const dynamicKeys: DynamicKeys = {
+          HASSYHHH: obj.HASSYHHH,
+          tmkkk: obj.tmkkk,
+          イキスギコード: obj.イキスギコード,
+          あたドン: obj.あたドン,
+          断水坊主: obj.断水坊主,
+          zuzunobu: obj.zuzunobu,
+        }; 
+        return new Row(obj.ID, obj.Date, obj.Timestamp, dynamicKeys);
+      });
   }
 }
