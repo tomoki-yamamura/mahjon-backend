467f78b96997f8d2152843ccca2e7385077e443b
tomoki-yamamura <progtab17@gmail.com>
2024-04-23 21:32:34 +0800

    delete user

--- src/entities/row.ts
+++ src/entities/row.ts
@@ -1,20 +1,14 @@
-import { Users } from "./users";
-
 export class Row {
   public readonly ID: string;
   public readonly Date: string;
   public readonly Timestamp: string;
-  public readonly Users: Partial<Record<Users, string>>
+  public readonly Users: Partial<Record<string, string>>
 
   constructor(obj: Partial<Record<string, any>>) {
     this.ID = obj.ID;
     this.Date = obj.Date;
     this.Timestamp = obj.Timestamp;
-    this.Users = {} as Partial<Record<Users, string>>;
-    for (const key in obj) {
-      if (key !== 'ID' && key !== 'Date' && key !== 'Timestamp') {
-        this.Users[key as Users] = obj[key] as string;
-      }
-    }
+    const users = (({ ID, Date, Timestamp, ...rest }) => rest)(obj);
+    this.Users = users
   }
 }
--- src/entities/sheet.ts
+++ src/entities/sheet.ts
@@ -3,9 +3,6 @@ import { Row } from "./row";
 export class Sheet {
   constructor(
     public readonly id: string,
-    // public readonly date: string,
-    // public readonly timeStamp: string,
-    // public readonly dynamicKeys: DynamicKeys
     public readonly rows: Row[]
   ) {}
 }
