6eee3a63a2334d3beda09ae04e719ce4fceb189c
tomoki-yamamura <progtab17@gmail.com>
2024-05-05 17:41:45 +0800

    type dto

--- src/interactors/dto/sheet.ts
+++ src/interactors/dto/sheet.ts
@@ -1,15 +1,7 @@
-import { Users } from "./users";
-
 export class Sheet {
   constructor(
     public readonly id: string,
-    // public readonly date: string,
-    // public readonly timeStamp: string,
-    // public readonly dynamicKeys: DynamicKeys
     public readonly rows: Partial<Record<string, any>>
   ) {}
 }
 
-type DynamicKeys = {
-  [key in Users]: string;
-};
--- src/interactors/sheetInteractor.ts
+++ src/interactors/sheetInteractor.ts
@@ -2,6 +2,7 @@ import { inject, injectable } from "inversify";
 import { ISheetInteractor } from "../interface/ISheetInteractor";
 import { ISheetRepository } from "../domain/repository/ISheetRepository";
 import TYPES from "../config/inversity.types";
+import { Sheet } from "./dto/sheet";
 
 @injectable()
 export class SheetInteractor implements ISheetInteractor {
@@ -19,7 +20,8 @@ export class SheetInteractor implements ISheetInteractor {
     startDate: string;
     endDate: string;
   }) {
-    const result = this.repository.querySheetByDateRange({id, startDate, endDate});
-    return result
+    const result = await this.repository.querySheetByDateRange({id, startDate, endDate});
+    const sheet = new Sheet(result.id, result.rows)
+    return sheet
   }
 }
