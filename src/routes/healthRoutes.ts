16c3bd84a7833638ebbbc84fb337eb8b3ea19eed
tomoki-yamamura <progtab17@gmail.com>
2024-05-23 20:15:56 +0800

    add swagger

--- src/routes/healthRoutes.ts
+++ src/routes/healthRoutes.ts
@@ -3,6 +3,17 @@ import TYPES from "../config/inversity.types";
 import container from "../config/inversity.config";
 import { HealthController } from "../controllers/healthController";
 
+/**
+ * @swagger
+ * /health:
+ *   get:
+ *     summary: Health check
+ *     responses:
+ *       200:
+ *         description: Returns a health check message.
+ */
+
+
 const router = exporess.Router();
 
 const controller = container.get<HealthController>(TYPES.HealthController);
