eeb86bf3d1718661a5a563fff4add8152df297d1
tomoki-yamamura <progtab17@gmail.com>
2024-06-15 16:25:52 +0800

    del scorelist

--- src/domain/entities/player.ts
+++ src/domain/entities/player.ts
@@ -8,14 +8,14 @@ class Player {
   readonly Id: string;
   readonly name: string;
   readonly scores: Score[];
-  constructor(id: string, name: string, scores: ScoreList) {
+  constructor(id: string, name: string, scores: Score[]) {
     this.Id = id
     this.name = name
     this.scores = scores
   }
   
   totalScores(): Point {
-    return this.scores.totalScorePoint()
+    return this.scores.reduce((acc, cur) => acc.add(cur.point), new Point(0))
   }
 
   getScoreByDate(date: PlayedDate): Score {
@@ -23,12 +23,12 @@ class Player {
   }
 
   filterScoresByDate(dateRange: PlayedDateRange): Player {
-    const filteredScoreList = this.scores.filter(dateRange)
+    const filteredScoreList = this.scores.filter((score) => dateRange.isWithinRange(score.date))
     return new Player(this.Id, this.name, filteredScoreList)
   }
 
   filterScoresByMode(mode: PlayMode): Player {
-    const filteredScoreList = this.scores.filterScoresByMode(mode)
+    const filteredScoreList = this.scores.filter((score) => score.mode.isEqualTo(mode))
     return new Player(this.Id, this.name, filteredScoreList)
   }
 }
